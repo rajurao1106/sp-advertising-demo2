4abfa10f032a130eff2ef383f4d1bf0196296d28
rajurao <rajurao1107@gmail.com>
2024-08-07 13:47:38 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router =  createBrowserRouter(createRoutesFromElements(
+  <App/>
+))
+
+ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter >
-    <App/>
+    <RouterProvider router={router}/>
   </BrowserRouter>
 );
